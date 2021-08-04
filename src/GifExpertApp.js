17434f684d06f5e4581b7b17c9389e2f165df4ce
Pera <esperanza.nave@gmail.com>
2021-08-03 23:55:24 -0600

    configure AddCategory and GifGrid, the first one show all categories and the second one show gifs from API

---
+++ src/GifExpertApp.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+   const [categories, setCategories] = useState(['One Puch']);
+
+    return (
+        <div>
+            <h1>GifExpertApp</h1>
+            <AddCategory setCategories={ setCategories } />
+            <hr />
+            
+            <ol>
+                {
+                    categories.map ( category =>(
+                        <GifGrid key={category} category={category} />
+                     ) )
+                }
+            </ol>
+        </div>
+    )
+}
