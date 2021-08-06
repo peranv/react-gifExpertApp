61181a6c9f6ded553435aee5171e055b45eeeb4a
Pera <esperanza.nave@gmail.com>
2021-08-06 10:19:10 -0600

    optimizing code

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,18 +1,22 @@
-import React, {useState, useEffect} from 'react';
+import React  from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem }   from './GifGridItem';
-import { getGif }       from '../helpers/getGif';
 
 export const GifGrid = ({category}) => {
 
-     const [images, setImages] = useState([]);
-     
+/* 
      useEffect( () => {
         getGif(category)
         .then(setImages);
-     },[category]);
+     }, [category]); */
+  ///data:images es para darle un nuevo nombre a data
+     const {data:images, loading} = useFetchGifs(category);
+    
+
     return (
        <>
          <h3> { category }</h3>
+              { loading && <p>loading</p> }
              <div className="card-grid">  
                 {
                     images.map( img=>(
